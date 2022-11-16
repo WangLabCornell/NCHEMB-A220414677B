1fc0267fd9dd9c3a97c055e14b09bf6dd2a00132
WangLabCornell <118195198+WangLabCornell@users.noreply.github.com>
2022-11-16 13:37:31 -0500

    Update data_statistic_stretching_all.m

--- constent velocity stretching/data_statistic_stretching_all.m
+++ constent velocity stretching/data_statistic_stretching_all.m
@@ -1,3 +1,11 @@
+% Copyright 2022 Wang Lab
+%
+% This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published % by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+
 function [peakforce_all, n_loop, name, dL_peak_mean, l0p5_a, fmax, ft, lt, pair_peak_width_all, peak_width_trace_all, location_break] = data_statistic_stretching_all            
                     
 %filename_t='force_kkb_1p6kbafterA20_100mMNaCl_4mMMg';
